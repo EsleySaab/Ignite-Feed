380dff111fa03010b4c5901770a3f9875b89f972
EsleySaab <esleysantana.sa@gmail.com>
2024-12-15 15:31:27 -0300

    adding styles to the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from "./App.module.css"
+
 import "./global.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a sapiente doloribus eum rerum recusandae eaque voluptatibus, consequuntur necessitatibus. Provident nobis optio iusto, mollitia suscipit quia nostrum cumque dolorem nemo."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a sapiente doloribus eum rerum recusandae eaque voluptatibus, consequuntur necessitatibus. Provident nobis optio iusto, mollitia suscipit quia nostrum cumque dolorem nemo."
+          />
 
-      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   )
 }
