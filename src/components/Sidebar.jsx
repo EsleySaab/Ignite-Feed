603a33f94ea71536eccaf6ba24934552ac7cb00b
EsleySaab <esleysantana.sa@gmail.com>
2024-12-16 21:14:19 -0300

    adding the avatar to the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react"
+
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -7,13 +9,18 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
+
       <div className={styles.profile}>
-        <strong>Diego Fernandes</strong>
+        <img className={styles.avatar} src="https://github.com/EsleySaab.png" />
+        <strong>Esley Santana</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
